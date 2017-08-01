d7f8be0578b53619c4c5313772906da2ddc1f17e
juanpinto <juan.pinto@rappi.com>
2017-08-01 09:25:15 -0500

    implementing methods

--- src/main/java/com/volunteer/dao/ProjectDao.java
+++ src/main/java/com/volunteer/dao/ProjectDao.java
@@ -3,6 +3,8 @@ package com.volunteer.dao;
 import java.util.List;
 
 import com.volunteer.model.Project;
+import com.volunteer.model.ResourceRequested;
+import com.volunteer.model.Status;
 import com.volunteer.model.Task;
 
 public interface ProjectDao {
@@ -11,9 +13,9 @@ public interface ProjectDao {
 	
 	List<Task> getProjectTasks(int projectId);
 	
-	List<Project> getProjectsByStatus();
+	List<Project> getProjectsByStatus(Status status);
 	
-	List<Project> getProjectsWithRequirement();
+	List<Project> getProjectsWithRequirement(ResourceRequested resource);
 	
 	List<Project> getProjectsWithKeyword(String key);
 	
--- src/main/java/com/volunteer/dao/ProjectDaoImpl.java
+++ src/main/java/com/volunteer/dao/ProjectDaoImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.persistence.Query;
 
 import com.volunteer.model.Project;
+import com.volunteer.model.ResourceRequested;
+import com.volunteer.model.Status;
 import com.volunteer.model.Task;
 
 public class ProjectDaoImpl extends JPADao<Project> implements ProjectDao{
@@ -18,31 +20,36 @@ public class ProjectDaoImpl extends JPADao<Project> implements ProjectDao{
 	@Override
 	public List<Task> getProjectTasks(int projectId) {
 		Query query = entityManager.createQuery("SELEC t from Project p JOIN FETCH p.tasks t WHERE p.id = :project_id");
+		query.setParameter("project_id", projectId);
 		return query.getResultList();
 	}
 
 	@Override
-	public List<Project> getProjectsByStatus() {
-		Query query = entityManager.createQuery("SELECT p from Project p JOIN FETCH ");
-		return null;
+	public List<Project> getProjectsByStatus(Status status) {
+		Query query = entityManager.createQuery("SELECT p from Project p WHERE p.status = :status");
+		query.setParameter("status", status);
+		return query.getResultList();
 	}
 
 	@Override
-	public List<Project> getProjectsWithRequirement() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Project> getProjectsWithRequirement(ResourceRequested resource) {
+		Query query = entityManager.createQuery("SELECT p from Project p JOIN FETCH p.resources r WHERE r = :resource");
+		query.setParameter("resource", resource);
+		return query.getResultList();
 	}
 
 	@Override
 	public List<Project> getProjectsWithKeyword(String key) {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = entityManager.createQuery("SELECT p from Project p WHERE p.description LIKE :key");
+		query.setParameter("key", key);
+		return query.getResultList();
 	}
 
 	@Override
 	public List<Project> getProjectsByLocation(String location) {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = entityManager.createQuery("SELECT p from Project p WHERE p.description LIKE :location");
+		query.setParameter("location", location);
+		return query.getResultList();
 	}
 
 }
